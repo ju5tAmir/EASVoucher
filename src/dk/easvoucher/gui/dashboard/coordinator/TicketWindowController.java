e6dd5c43d871ba11cbe129910ab1391801c57ad3
YehorNe1 <yehornechai1@gmail.com>
2024-03-22 16:54:34 +0100

    TicketController
    added combo boxes with inserted data

--- src/dk/easvoucher/gui/dashboard/coordinator/TicketWindowController.java
+++ src/dk/easvoucher/gui/dashboard/coordinator/TicketWindowController.java
@@ -12,11 +12,16 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TicketWindowController {
     public TextField ticketNumberField, qrCodeField, barcodeField, uuidField;
-    public ComboBox<Integer> ticketTypeField, eventField, customerField;
+    public ComboBox<String> ticketTypeField, eventField, customerField;
+    private Map<String, Integer> ticketTypeMap = new HashMap<>();
+    private Map<String, Integer> eventTypeMap = new HashMap<>();
+    private Map<String, Integer> customerTypeMap = new HashMap<>();
     public TableView<Ticket> ticketsTable;
     public TableColumn<Ticket, Integer> ticketIdColumn;
     public TableColumn<Ticket, String> ticketNumberColumn, qrCodeColumn, barcodeColumn, uuidColumn;
@@ -26,12 +31,31 @@ public class TicketWindowController {
 
     public TicketWindowController() {
         model = new Model();
+
+        ticketTypeMap.put("Participant", 1);
+        ticketTypeMap.put("Customizable", 2);
+        ticketTypeMap.put("VIP ticket", 3);
+        ticketTypeMap.put("1st row", 4);
+        ticketTypeMap.put("free beer", 5);
+
+        eventTypeMap.put("EASV Party", 1);
+
+        customerTypeMap.put("Customer1", 1);
     }
 
     public void initialize() throws SQLException {
         setTable();
         setColumns();
         refreshTickets();
+
+        ObservableList<String> ticketTypes = FXCollections.observableArrayList(ticketTypeMap.keySet());
+        ticketTypeField.setItems(ticketTypes);
+
+        ObservableList<String> eventTypes = FXCollections.observableArrayList(eventTypeMap.keySet());
+        eventField.setItems(eventTypes);
+
+        ObservableList<String> customers = FXCollections.observableArrayList(customerTypeMap.keySet());
+        customerField.setItems(customers);
     }
 
 
@@ -39,7 +63,6 @@ public class TicketWindowController {
         List<Ticket> tickets = model.getAllTickets();
         ObservableList<Ticket> observableList= FXCollections.observableArrayList(tickets);
         ticketsTable.setItems(observableList);
-
     }
 
     private void setColumns(){
@@ -58,9 +81,15 @@ public class TicketWindowController {
             String qrCode = qrCodeField.getText();
             String barcode = barcodeField.getText();
             String uuid = uuidField.getText();
-            Integer ticketTypeId = ticketTypeField.getValue();
-            Integer eventId = eventField.getValue();
-            Integer customerId = customerField.getValue();
+
+            String ticketTypeString = ticketTypeField.getValue();
+            Integer ticketTypeId = ticketTypeMap.get(ticketTypeString);
+
+            String eventTypeString = eventField.getValue();
+            Integer eventId = eventTypeMap.get(eventTypeString);
+
+            String customerString = customerField.getValue();
+            Integer customerId = customerTypeMap.get(customerString);
 
             model.insertTicket(ticketNumber, qrCode, barcode, uuid, ticketTypeId, eventId, customerId);
             refreshTickets();
@@ -73,7 +102,21 @@ public class TicketWindowController {
         Ticket selectedTicket = ticketsTable.getSelectionModel().getSelectedItem();
         if (selectedTicket != null) {
             try {
-                model.updateTicket(selectedTicket.getTicketId(), ticketNumberField.getText(), qrCodeField.getText(), barcodeField.getText(), uuidField.getText(), ticketTypeField.getValue(), eventField.getValue(), customerField.getValue());
+                String ticketNumber = ticketNumberField.getText();
+                String qrCode = qrCodeField.getText();
+                String barcode = barcodeField.getText();
+                String uuid = uuidField.getText();
+
+                String ticketTypeString = ticketTypeField.getValue();
+                Integer ticketTypeId = ticketTypeMap.get(ticketTypeString);
+
+                String eventTypeString = eventField.getValue();
+                Integer eventId = eventTypeMap.get(eventTypeString);
+
+                String customerString = customerField.getValue();
+                Integer customerId = customerTypeMap.get(customerString);
+
+                model.updateTicket(selectedTicket.getTicketId(), ticketNumber, qrCode, barcode, uuid, ticketTypeId, eventId, customerId);
                 refreshTickets();
             } catch (SQLException e) {
                 e.printStackTrace();
